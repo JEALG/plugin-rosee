cf85bdcc9605d0bedf7041c1cb1b1c5a0da6fb79
JEALG <jalain.gosse@icloud.com>
2020-04-17 15:21:29 +0200

    Cron

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -35,8 +35,8 @@ if (!isConnect()) {
     $("input[data-l1key='functionality::cron30::enable']").on('change', function() {
         if ($(this).is(':checked')) $("input[data-l1key='functionality::cron5::enable']").prop("checked", false)
     });
-    
-    $("input[data-l1key='functionality::cronHourly::enable']").on('change', function() {
+
+    $("input[data-l1key='functionality::cronH::enable']").on('change', function() {
         if ($(this).is(':checked')) $("input[data-l1key='functionality::cron5::enable']").prop("checked", false)
     });
 
