c1366dc0337d56e3197f11f0d53ee03df5ca825c
JEALG <jalain.gosse@icloud.com>
2020-04-18 11:24:13 +0200

    Bug pression

--- core/class/rosee.class.php
+++ core/class/rosee.class.php
@@ -352,7 +352,7 @@ class rosee extends eqLogic {
         }
 
         /*  ********************** PRESSION *************************** */
-        $pressure = $this->getConfiguration('pressure');
+        $pressure = $this->getConfiguration('pression');
         if ($pressure == '' && $calcul !='tendance') {//valeur par défaut de la pression atmosphérique : 1013.25 hPa
             $pressure=1013.25;
             log::add('rosee', 'debug', '│ Pression Atmosphérique aucun équipement sélectionné, valeur par défaut : '. $pressure. ' hPa');
@@ -364,7 +364,7 @@ class rosee extends eqLogic {
                 log::add('rosee', 'debug', '│ Pression Atmosphérique : ' . $pressure.' hPa');
             } else {
                 throw new Exception(__('Le champ "Pression Atmosphérique" ne peut être vide',__FILE__));
-                log::add('rosee', 'error', '│ Configuration : Pression Atmosphérique inexistante : ' . $this->getConfiguration('pressure'));
+                log::add('rosee', 'error', '│ Configuration : Pression Atmosphérique inexistante : ' . $this->getConfiguration('pression'));
             }
         }
 
