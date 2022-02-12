8ed34f58f637d5acb2697ebbc21c07b86e0f6383
JEALG <jalain.gosse@icloud.com>
2022-02-12 08:05:39 +0100

    Methode deadCmd

--- core/class/rosee.class.php
+++ core/class/rosee.class.php
@@ -27,7 +27,24 @@ class rosee extends eqLogic
     /*     * ***********************Methode static*************************** */
     public static function deadCmd()
     {
-        return array();
+        $return = array();
+        foreach (eqLogic::byType('rosee') as $rosee) {
+            foreach ($rosee->getCmd() as $cmd) {
+                preg_match_all("/#([0-9]*)#/", $cmd->getConfiguration('infoName', ''), $matches);
+                foreach ($matches[1] as $cmd_id) {
+                    if (!cmd::byId(str_replace('#', '', $cmd_id))) {
+                        $return[] = array('detail' => __('rosee', __FILE__) . ' ' . $rosee->getHumanName() . ' ' . __('dans la commande', __FILE__) . ' ' . $cmd->getName(), 'help' => __('Nom Information', __FILE__), 'who' => '#' . $cmd_id . '#');
+                    }
+                }
+                preg_match_all("/#([0-9]*)#/", $cmd->getConfiguration('calcul', ''), $matches);
+                foreach ($matches[1] as $cmd_id) {
+                    if (!cmd::byId(str_replace('#', '', $cmd_id))) {
+                        $return[] = array('detail' => __('rosee', __FILE__) . ' ' . $rosee->getHumanName() . ' ' . __('dans la commande', __FILE__) . ' ' . $cmd->getName(), 'help' => __('Calcul', __FILE__), 'who' => '#' . $cmd_id . '#');
+                    }
+                }
+            }
+        }
+        return $return;
     }
     public static $_widgetPossibility = array('custom' => true);
     public static function cron5($_eqlogic_id = null)
